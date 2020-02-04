a6f01f528c802f2bf2d71f99a1daad6451ed1770
Lois Fuller <57360970+loisfuller2019@users.noreply.github.com>
2020-02-04 17:40:07 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,214 @@
+function setup() {
+  createCanvas(500, 500, WEBGL)
+}
+
+
+function draw() {
+  
+
+  
+fill('#fae');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  cylinder(10, 400);
+//pink
+
+ colorMode(HSB);
+fill(255, 204, 100);
+cylinder(20, 20, 60, 60);
+rotateX(frameCount * 0.03);
+  rotateZ(frameCount * 0.03);
+  cylinder(10, 400);
+  // blue
+
+fill('#fae');
+cylinder(20, 20, 60, 60);
+ rotateX(frameCount * 0.02);
+  rotateZ(frameCount * 0.02);
+  cylinder(10, 400);
+ // pink
+  
+  fill('rgb(0,255,0)');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.01);
+  rotateX(frameCount * 0.01);
+  cylinder(10, 400);
+  // green 
+  
+  fill('red');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.090);
+  rotateX(frameCount * 0.090);
+  cylinder(10, 400);
+//red
+  
+  
+  fill('orange');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.200);
+  rotateX(frameCount * 0.200);
+  cylinder(20, 400);
+ //orange
+
+    fill('yellow');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(10, 400);
+  
+      fill('purple');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.04);
+  rotateY(frameCount * 0.04);
+  cylinder(10, 400);
+  
+        fill('blue');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
+  cylinder(10, 400);
+  
+  
+    fill('pink');
+cylinder(20, 20, 60, 60);
+  rotateY(frameCount * 0.08);
+  rotateY(frameCount * 0.05);
+  cylinder(10, 400);
+  
+  
+    fill('green');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.04);
+  cylinder(10, 400); 
+  
+  
+    fill('red');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.02);
+  cylinder(10, 400);
+  
+    fill('yellow');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.10);
+  rotateY(frameCount * 0.20);
+  cylinder(10, 400);
+  
+    fill('skyblue');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.10);
+  rotateX(frameCount * 0.20);
+  cylinder(10, 400);
+  
+    fill('white');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.200);
+  rotateY(frameCount * 0.20);
+  cylinder(10, 400);
+  
+    fill('orange');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.1000);
+  rotateX(frameCount * 0.1000);
+  cylinder(10, 400);
+
+    fill('aqua');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.500);
+  rotateX(frameCount * 0.500);
+  cylinder(20, 300);
+  
+  
+    fill('blueViolet');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.900);
+  rotateY(frameCount * 0.900);
+  cylinder(10, 300);
+ 
+    fill('chartreuse');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.1000);
+  rotateY(frameCount * 0.1000);
+  cylinder(12, 400);
+  
+    fill('coral');
+cylinder(20, 20, 60, 60);
+   rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(10, 400);
+  
+    fill('white');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.05);
+  rotateY(frameCount * 0.05);
+  cylinder(13, 400);
+  
+  
+    fill('crimson');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.20);
+  rotateY(frameCount * 0.20);
+  cylinder(18, 400);
+  
+  
+    fill('DarkMagenta');
+cylinder(20, 20, 60, 60);
+    rotateY(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  cylinder(12, 400);  
+  
+    fill('deepPink');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.20);
+  rotateY(frameCount * 0.20);
+  cylinder(18, 400);
+  
+    fill('gold');
+cylinder(20, 20, 60, 60);
+    rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  cylinder(9, 400);
+  
+    fill('blue');
+cylinder(20, 20, 60, 60);
+    rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  cylinder(9, 400);
+  
+    fill('greenYellow');
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.02);
+  rotateY(frameCount * 0.02);
+  cylinder(9, 400);
+  
+
+  fill('orangeRed');
+cylinder(20, 20, 60, 60);
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.60);
+  rotateY(frameCount * 0.60);
+  cylinder(13, 400);
+  
+  
+ fill('white');
+cylinder(20, 20, 60, 60);
+cylinder(20, 20, 60, 60);
+  rotateX(frameCount * 0.60);
+  rotateY(frameCount * 0.60);
+  cylinder(13, 400);
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
